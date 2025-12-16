8d5888bf59fe53dee29910e2a984b47a9649007e
Jakub Bartczuk <bartczukkuba@gmail.com>
2025-12-16 16:02:57 +0100

    fixed goodfire SAE feature label loading

---
+++ test/test_feature_labels.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from interp_embed.sae.local_sae import GoodfireSAE
+
+
+def test_goodfire_sae_labels():
+    goodfire_sae = GoodfireSAE(
+        variant_name="Llama-3.1-8B-Instruct-SAE-l19",
+    )
+    assert len(goodfire_sae.feature_labels()) > 0
